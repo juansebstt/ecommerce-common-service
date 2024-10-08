af0369f2bbf8f15237c29670ecdc988b7efa031b
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-08 14:19:09 -0500

    first commit

---
+++ src/test/java/com/ecommercecommonservice/EcommerceCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercecommonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCommonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
